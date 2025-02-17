6e6f403ff1c9b508a061eae9cba5713b167cc4f6
Rhineland905 <104513373+Rhineland905@users.noreply.github.com>
2025-02-17 17:27:00 +0200

    Super admin commands

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from telebot.types import BotCommand, BotCommandScopeDefault, BotCommandScopeCha
 
 
 
-my_bot = telebot.TeleBot('5116218178:AAHThqvo_5BpxJv8L_kpgrM_a7LQPZQEfI4')
+my_bot = telebot.TeleBot('5116218178:AAEASMZ5hfpBK0P5Swkq-p5jZDI49Qf-jL0')
 
 config_tg = {
     'user': 'root',
@@ -23,16 +23,14 @@ markup.add(button)
 def set_bot_commands():
     user_commands = [
         BotCommand("text", "text"),
-        BotCommand("help", "Допомога"),
-        BotCommand("about", "Про бота")
     ]
     my_bot.set_my_commands(user_commands, scope=BotCommandScopeDefault())
     super_admin_id = [534670150,565948862]
     super_admin = [
-        BotCommand("admin", "Видавваня адмінки"),
-        BotCommand("help", "Допомога"),
-        BotCommand("about", "Про бота")
+        BotCommand("admin_give", "Видавваня адмінки"),
+        BotCommand("admin_delete", "Відібрати адмінку"),
     ]
+
     for admin_id in super_admin_id:
         my_bot.set_my_commands(super_admin, scope=BotCommandScopeChat(admin_id))
 
@@ -40,6 +38,81 @@ set_bot_commands()
 def is_valid_name(text):
     pattern = r'^[А-Яа-яЇїІіЄєҐґ]{2,}$'
     return bool(re.match(pattern, text))
+def process_admin_delete(message):
+    id = message.from_user.id
+    if id == 534670150 or id == 565948862:
+        text = message.text
+        parts = text.split(maxsplit=1)
+        if len(parts) == 1:
+            id = parts[0]
+            with conn_tg.cursor() as curs:
+                curs.execute(f'SELECT * FROM  user WHERE user_id=%s', (str(id),))
+                user_data = curs.fetchall()
+            if not user_data:
+                my_bot.send_message(message.chat.id, "❌ Такого користувача не існує!")
+            elif not user_data[0][4]:
+                my_bot.send_message(message.chat.id, f"Користувачь не адміністратор")
+            else:
+                with conn_tg.cursor() as curs:
+                    curs.execute("UPDATE user SET admin=%s WHERE user_id=%s", (False, str(id)))
+                    conn_tg.commit()
+                my_bot.send_message(message.chat.id, f"✅ Користувачу {user_data[0][2]} {user_data[0][3]} відібрано адмінку!")
+        elif len(parts) == 2:
+            first_name, last_name = parts[0], parts[1]
+            with conn_tg.cursor() as curs:
+                curs.execute(f'SELECT * FROM user WHERE first_name=%s AND last_name=%s', (first_name, last_name))
+                user_data = curs.fetchall()
+            if not user_data:
+                my_bot.send_message(message.chat.id, "❌ Такого користувача не існує!")
+            elif not user_data[0][4]:
+                my_bot.send_message(message.chat.id, "Користувачь не адміністратор")
+            else:
+                with conn_tg.cursor() as curs:
+                    curs.execute("UPDATE user SET admin=%s WHERE first_name=%s AND last_name=%s",(False, first_name, last_name))
+                    conn_tg.commit()
+                my_bot.send_message(message.chat.id,f"✅ Користувачу {user_data[0][2]} {user_data[0][3]} відібрано адмінку!")
+
+    else:
+        my_bot.send_message(message.chat.id, "Як ти дізнавс про це! У тебе немае прав для цього")
+
+def process_admin_give(message):
+    id = message.from_user.id
+    if id == 534670150 or id == 565948862:
+        text = message.text
+        parts = text.split(maxsplit=1)
+        if len(parts) == 1:
+            id = parts[0]
+            with conn_tg.cursor() as curs:
+                curs.execute(f'SELECT * FROM  user WHERE user_id=%s', (str(id),))
+                user_data = curs.fetchall()
+            if not user_data:
+                my_bot.send_message(message.chat.id, "❌ Такого користувача не існує!")
+            elif user_data[0][4]:
+                my_bot.send_message(message.chat.id, f"Вже адміністратор")
+            else:
+                with conn_tg.cursor() as curs:
+                    curs.execute("UPDATE user SET admin=%s WHERE user_id=%s", (True,str(id)))
+                    conn_tg.commit()
+                my_bot.send_message(message.chat.id, f"✅ У користувачу {user_data[0][2]} {user_data[0][3]} видано адмінку!")
+        elif len(parts) == 2:
+            first_name, last_name = parts[0], parts[1]
+            with conn_tg.cursor() as curs:
+                curs.execute(f'SELECT * FROM user WHERE first_name=%s AND last_name=%s', (first_name, last_name))
+                user_data = curs.fetchall()
+            if not user_data:
+                my_bot.send_message(message.chat.id, "❌ Такого користувача не існує!")
+            elif user_data[0][4]:
+                my_bot.send_message(message.chat.id, "Вже адміністратор")
+            else:
+                with conn_tg.cursor() as curs:
+                    curs.execute("UPDATE user SET admin=%s WHERE first_name=%s AND last_name=%s",(True, first_name, last_name))
+                    conn_tg.commit()
+                my_bot.send_message(message.chat.id,f"✅ У користувачу {user_data[0][2]} {user_data[0][3]} видано адмінку!")
+
+    else:
+        my_bot.send_message(message.chat.id, "Як ти дізнавс про це! У тебе немае прав для цього")
+
+
 def firts_last_name(message):
     id = message.from_user.id
     text = message.text
@@ -53,9 +126,16 @@ def firts_last_name(message):
         my_bot.send_message(message.chat.id, "Ви ввели неправильне ім'я або фамілю")
         my_bot.register_next_step_handler(message, firts_last_name)
 
-@my_bot.message_handler(commands=['menu'])
-def admin(message):
-    pass
+@my_bot.message_handler(commands=['admin_give'])
+def admin_give(message):
+    my_bot.send_message(message.chat.id, f"Ведіть Ім'я прізвище або id")
+    my_bot.register_next_step_handler(message, process_admin_give)
+
+@my_bot.message_handler(commands=['admin_delete'])
+def admin_give(message):
+    my_bot.send_message(message.chat.id, f"Ведіть Ім'я прізвище або id")
+    my_bot.register_next_step_handler(message, process_admin_delete)
+
 
 @my_bot.message_handler(commands=['start'])
 def start(message):
